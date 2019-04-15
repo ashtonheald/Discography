fb311107a2ec26f4cc4a4fa88b3b5d235407be7d
ashtonheald <ashfalltv@yahoo.com>
2019-04-15 08:28:58 -0500

    connection to database

---
+++ discography/includes/connection.php
@@ -0,0 +1,10 @@
+<?php 
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "Agh1013132_";
+$dbName = "the_killers_discography";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+?>
